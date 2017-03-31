2871078ba22a2286ca2ad3b097a725233da8dcb6
matts90 <seilermr@dukes.jmu.edu>
2017-03-31 10:39:26 -0400

    Map update

--- index.php
+++ index.php
@@ -145,7 +145,7 @@ function verifyLogin(){
               <input type="number" id="mileage" class="form-control" value="" name="mileage" required="required" placeholder="Mileage" />
             </div>
           </div>
-          <div><a target="_blank" href="https://www.google.com/maps/dir//Wildlife+Center+of+VA,+George+Washington+and+Jefferson+National+Forest,+1800+S+Delphine+Ave,+Waynesboro,+VA+22980/@38.0392567,-78.9159496,17z/data=!4m15!1m6!3m5!1s0x89b3640efe4889c9:0xf47416d422aa90a9!2sWildlife+Center+of+VA!8m2!3d38.0392567!4d-78.9137609!4m7!1m0!1m5!1m1!1s0x89b3640efe4889c9:0xf47416d422aa90a9!2m2!1d-78.9137609!2d38.0392567">Click here to check how far you drove.</a></div>
+          <div><a target="_blank" href="https://www.google.com/maps/dir//Wildlife+Center+of+VA,+South+Delphine+Avenue,+Waynesboro,+VA///@38.0392515,-78.9487804,13z/data=!4m11!4m10!1m0!1m5!1m1!1s0x89b3640efe4889c9:0xf47416d422aa90a9!2m2!1d-78.9137609!2d38.0392567!1m0!1m0!3e0">Click here to check how far you drove.</a></div>
           <br>
           <div class="form-group">
             <div class="col-sm-2 col-sm-offset-4">
