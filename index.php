b1baab1a775d390e3af11f22a794a53cce18f0d0
dannymahoney <mahonedb@dukes.jmu.edu>
2017-03-31 16:09:08 +0000

    index update with confirmation page
    added confimation for apply section

--- index.php
+++ index.php
@@ -248,30 +248,30 @@ require 'C:\inetpub\wwwroot\PHPMailer\PHPMailerAutoload.php';
 $mail = new PHPMailer;
 
 $mail->IsSMTP();
-$mail->Host = "smtp.gmail.com";
+$mail->Host = "smtp.gmail.com"; // may need to change host depending on which email client we are using
 $mail->SMTPAuth = true;
 $mail->SMTPSecure = "ssl";
-$mail->Username = "dannymahoney33@gmail.com";
-$mail->Password = "enter a password here to test with (alter email as well)";
-$mail->Port = "465";
+$mail->Username = "<enter wildife email here>";  // the email we want to send from (wildlife email)
+$mail->Password = "<enter password here>"; // above emails password
+$mail->Port = "465"; // may need to change port depending on which email client we are using
 
 
-$mail->setFrom('dannymahoney33@gmail.com', 'Wildlife Center of Virginia');
-$mail->addAddress($emailAddress, 'Danny Mahoney');     // recipient
+$mail->setFrom('<enter wildlife email here>', 'Wildlife Center of Virginia');
+$mail->addAddress($emailAddress, 'Applicant');     // recipient
 
 $mail->Subject = 'Wildlife Center of Virginia Volunteer Opportunity';
-$mail->Body    = 'Thank you for your interest in the Wildlife Center!<br />
+$mail->Body    = 'Thank you for your interest in the Wildlife Center!<br>
                   Please follow the link provided to fill out an application.
-                  <br />';
+                  ';
 
 if(!$mail->send()) {
    echo 'Message could not be sent.';
     echo 'Mailer Error: ' . $mail->ErrorInfo;
 }
  else {
-	 /* This would be for when they fill out an application, not for when they enter they email to get a link sent to them********************************
-  echo "<script>window.top.location='confirmation.php'</script>";
-  */
+	  // This would be for when they fill out an application, not for when they enter they email to get a link sent to them********************************
+  echo "<script>window.top.location='apply_confirmation.php'</script>";
+  
 }
 
 
