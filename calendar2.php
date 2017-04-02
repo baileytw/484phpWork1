84368a6cf7a00e25fc578dacc070dffc6f462f4b
jaidon021 <beckneam@dukes.jmu.edu>
2017-04-02 17:47:10 -0400

    a
    a

--- calendar2.php
+++ calendar2.php
@@ -173,13 +173,13 @@ $events = $req->fetchAll();
 				  <div class="form-group">
 					<label for="start" class="col-sm-2 control-label">Start date:</label>
 					<div class="col-sm-10">
-					  <input pattern=".{19,19}" type="text" name="start" class="form-control" id="start" required placeholder="YYYY-MM-DD HH:MM:SS">
+					  <input pattern=".{19,19}" type="datetime-local" name="start" class="form-control" id="start" required placeholder="YYYY-MM-DD HH:MM:SS">
 					</div>
 				  </div>
 				  <div class="form-group">
 					<label for="end" class="col-sm-2 control-label">End date:</label>
 					<div class="col-sm-10">
-					  <input pattern=".{19,19}"type="text" name="end" class="form-control" id="end" required>
+					  <input pattern=".{19,19}"type="datetime-local" name="end" class="form-control" id="end" required>
 					</div>
 				  </div>
 				  
