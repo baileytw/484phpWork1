4822a5261fb0f41b0ace2bdeca21a310f114d31d
Zach Curry <curry.zacha@gmail.com>
2017-04-05 12:46:30 -0400

    ignore
    added white space, Noticed im not editing the page I want to

--- editAccount.php
+++ editAccount.php
@@ -1,9 +1,17 @@
 <!DOCTYPE html>
 <?php
+
+
+
+
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 ?>
 <?php
+
+
+
+
 //Session variables: KEEP AT TOP
 session_start();
 $userID = $_SESSION['userID'];
@@ -114,17 +122,17 @@ if(isset($_POST['btnSave']))
 
 <title>Wildlife Center of Virginia Volunteers</title>
 
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
 
-<link rel="stylesheet" media="screen" href="css/style.css" />
+	<link rel="stylesheet" media="screen" href="css/style.css" />
 
 </head>
 <body>
@@ -141,7 +149,6 @@ if(isset($_POST['btnSave']))
                       </button>
                       <a class="navbar-brand" href="calendar2.php"><img src="../484phpWork1/images/logo_short.png" alt="Wildlife Small Logo"></a>
                     </div>
-    
                   
                     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
