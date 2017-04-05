a065552f2ea6956b73dd8c8722380d79911135e6
Zach Curry <curry.zacha@gmail.com>
2017-04-04 22:52:02 -0400

    PHP formatting
    Deleted the Age textbox, don't need it if we have date of birth

--- outreach.php
+++ outreach.php
@@ -133,12 +133,6 @@ error_reporting(E_ALL);
                                                     </div>
                                                 </div>
                                                 <div class="form-group">
-                                                    <label class="col-sm-2">Age *</label>
-                                                    <div class="col-sm-10">
-                                                      <input class="form-control" id = 'age' type="number" name="age" value="<?php if (isset($_POST['upload'])) echo ($_POST['age']);?>" required="required" value="0" />
-                                                    </div>
-                                                </div>
-												<div class="form-group">
                                                     <label class="col-sm-2">Address *</label>
                                                     <div class="col-sm-10">
                                                       <input class="form-control" id= 'address' name="address" type="text" value="<?php if (isset($_POST['upload'])) echo ($_POST['address']);?>" placeholder="street" name="street" />
@@ -153,58 +147,58 @@ error_reporting(E_ALL);
                                                     <label class="col-sm-1">State *</label>
                                                     <div class="col-sm-3">
 													
-													<select name="state">
-													<option value="VA">Virginia</option>
-<option value="AL">Alabama</option>
-<option value="AK">Alaska</option>
-<option value="AZ">Arizona</option>
-<option value="AR">Arkansas</option>
-<option value="CA">California</option>
-<option value="CO">Colorado</option>
-<option value="CT">Connecticut</option>
-<option value="DE">Delaware</option>
-<option value="FL">Florida</option>
-<option value="GA">Georgia</option>
-<option value="HI">Hawaii</option>
-<option value="ID">Idaho</option>
-<option value="IL">Illinois</option>
-<option value="IN">Indiana</option>
-<option value="IA">Iowa</option>
-<option value="KS">Kansas</option>
-<option value="KY">Kentucky</option>
-<option value="LA">Louisiana</option>
-<option value="ME">Maine</option>
-<option value="MD">Maryland</option>
-<option value="MA">Massachusetts</option>
-<option value="MI">Michigan</option>
-<option value="MN">Minnesota</option>
-<option value="MS">Mississippi</option>
-<option value="MO">Missouri</option>
-<option value="MT">Montana</option>
-<option value="NE">Nebraska</option>
-<option value="NV">Nevada</option>
-<option value="NH">New Hampshire</option>
-<option value="NJ">New Jersey</option>
-<option value="NM">New Mexico</option>
-<option value="NY">New York</option>
-<option value="NC">North Carolina</option>
-<option value="ND">North Dakota</option>
-<option value="OH">Ohio</option>
-<option value="OK">Oklahoma</option>
-<option value="OR">Oregon</option>
-<option value="PA">Pennsylvania</option>
-<option value="RI">Rhode Island</option>
-<option value="SC">South Carolina</option>
-<option value="SD">South Dakota</option>
-<option value="TN">Tennessee</option>
-<option value="TX">Texas</option>
-<option value="UT">Utah</option>
-<option value="VT">Vermont</option>
-<option value="WA">Washington</option>
-<option value="WV">West Virginia</option>
-<option value="WI">Wisconsin</option>
-<option value="WY">Wyoming</option>
-</select> 
+														<select name="state">
+															<option value="VA">Virginia</option>
+															<option value="AL">Alabama</option>
+															<option value="AK">Alaska</option>
+															<option value="AZ">Arizona</option>
+															<option value="AR">Arkansas</option>
+															<option value="CA">California</option>
+															<option value="CO">Colorado</option>
+															<option value="CT">Connecticut</option>
+															<option value="DE">Delaware</option>
+															<option value="FL">Florida</option>
+															<option value="GA">Georgia</option>
+															<option value="HI">Hawaii</option>
+															<option value="ID">Idaho</option>
+															<option value="IL">Illinois</option>
+															<option value="IN">Indiana</option>
+															<option value="IA">Iowa</option>
+															<option value="KS">Kansas</option>
+															<option value="KY">Kentucky</option>
+															<option value="LA">Louisiana</option>
+															<option value="ME">Maine</option>
+															<option value="MD">Maryland</option>
+															<option value="MA">Massachusetts</option>
+															<option value="MI">Michigan</option>
+															<option value="MN">Minnesota</option>
+															<option value="MS">Mississippi</option>
+															<option value="MO">Missouri</option>
+															<option value="MT">Montana</option>
+															<option value="NE">Nebraska</option>
+															<option value="NV">Nevada</option>
+															<option value="NH">New Hampshire</option>
+															<option value="NJ">New Jersey</option>
+															<option value="NM">New Mexico</option>
+															<option value="NY">New York</option>
+															<option value="NC">North Carolina</option>
+															<option value="ND">North Dakota</option>
+															<option value="OH">Ohio</option>
+															<option value="OK">Oklahoma</option>
+															<option value="OR">Oregon</option>
+															<option value="PA">Pennsylvania</option>
+															<option value="RI">Rhode Island</option>
+															<option value="SC">South Carolina</option>
+															<option value="SD">South Dakota</option>
+															<option value="TN">Tennessee</option>
+															<option value="TX">Texas</option>
+															<option value="UT">Utah</option>
+															<option value="VT">Vermont</option>
+															<option value="WA">Washington</option>
+															<option value="WV">West Virginia</option>
+															<option value="WI">Wisconsin</option>
+															<option value="WY">Wyoming</option>
+														</select> 
                                                     </div>
                                             
                                                     <label class="col-sm-2">Zip Code *</label>
