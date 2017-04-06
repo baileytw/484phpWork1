84969029373fb2f6658eef4d4965a9775814799f
baileytw <twbailey94@gmail.com>
2017-04-06 02:33:06 +0000

    doesnt work

--- transporterapp.php
+++ transporterapp.php
@@ -66,7 +66,7 @@
                                 </header>
                                 <section class="panel-body container-fluid">
                                     <div class="leading">
-										<form id="form" class="form-horizontal panel panel-default">
+										<form id="form" method ="post" enctype = "multipart/form-data" class="form-horizontal panel panel-default">
                                             <header class="panel-heading"><h2 class="panel-title">Basic Information</h2></header>
             
                                             <fieldset class="panel-body">
@@ -315,7 +315,7 @@
 												<div class="form-group">
 													<label class="col-sm-3">If so, how recently? Please provide proof of vaccination. Upload an attachment.</label>
 													<div>
-														<select name="DOBMonth">
+														<select name="VACMonth">
 															<option>- Month -</option>
 															<option value="01">January</option>
 															<option value="02">February</option>
@@ -330,7 +330,7 @@
 															<option value="11">November</option>
 															<option value="12">December</option>
 														</select>
-														<select name="DOBDay">
+														<select name="VACDay">
 															<option>- Day -</option>
 															<option value="01">1</option>
 															<option value="02">2</option>
@@ -364,7 +364,7 @@
 															<option value="30">30</option>
 															<option value="31">31</option>
 														</select>
-														<select name="DOBYear">
+														<select name="VACYear">
 															<option> - Year - </option>
 															<option value="2017">2017</option>
 															<option value="2016">2016</option>
@@ -404,7 +404,7 @@
 												</div>
 												<label class="col-sm-6">How far are you willing to travel for transport (i.e., 30-45 miles from your location, to a specific location, etc)?</label>
 												<div class="col-sm-6">
-													<textarea class="form-control" rows="5" cols="90"></textarea>
+													<textarea class="form-control" name = "travel" rows="5" cols="90"></textarea>
 												</div>
 												<div class="form-group">
 													<label class="col-sm-12"><center><h3>Capturing Wildlife</h3></center></label>
@@ -434,14 +434,14 @@
 														<label class="col-sm-5">Transporter Guidelines</label>
 														<div class="col-sm-7">
 															<div class="checkbox">
-																<input type="radio" name="" value=""> I am acknowledging that I have read the transporter guidelines and I promise to abide by these guidelines when I transport for the Wildlife Center of Virginia.
+																<input type="radio" name="acknowledge" value=""> I am acknowledging that I have read the transporter guidelines and I promise to abide by these guidelines when I transport for the Wildlife Center of Virginia.
 															</div>
 														</div>
 													</div>
 												</div>
 											</fieldset>
 											<footer class="panel-footer">
-												<button class="btn btn-primary" type="submit">Submit form</button>
+												<button class="btn btn-primary" type="submit" name = 'upload' id = 'upload'>Submit form</button>
 												<button class="btn btn-default" type="reset">Reset</button>
 											</footer>
 										</form>
@@ -449,6 +449,7 @@
 								</section>
 							</div>
 						</div>
+
                     <!-- Main Section End -->
 
                 
@@ -466,6 +467,163 @@
 				</footer>
 			</div>
 		</section>
+		<?php
+		
+	  if(isset($_POST['upload']))
+	{
+		if($_POST['password'] == $_POST['check']){
+	
+	
+		/****************************************
+			START PASWWORD CODE 
+		****************************************/
+
+		require("PasswordHash.php");
+		$hasher = new PasswordHash(8, false);
+		// Retrieve password
+		$password = $_POST["password"];
+		// Limit passwords to 72 characters to help prevent DoS attacks
+		if (strlen($password) > 72) { die("Password must be 72 characters or less"); }
+		// The $hash variable will contain the hash of the password
+		$hash = $hasher->HashPassword($password);
+		if (strlen($hash) >= 20) {
+			$passwordHashPassed = $hash;
+				
+		} else {
+			
+		 // something went wrong
+
+		}
+
+		/****************************************
+			END PASWWORD CODE 
+		****************************************/
+		
+	  $server = "localhost";
+	  $user = "root";
+	  $password = "Twspike1994?";
+	  $database = "wildlife";
+
+	  $conn = mysqli_connect($server, $user, $password, $database);
+			if (mysqli_connect_errno()) 
+			{
+		  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+			}
+
+		  if(!mysqli_select_db($conn, 'wildlife'))
+			{
+				echo "Database Not Selected";
+			}
+
+		 
+
+			$firstName = $_POST['firstName'];
+			$lastName = $_POST['lastName'];
+			$userName = $_POST['email'];
+			$passwordHash = $passwordHashPassed;
+			$userType = "Applicant";
+			$email = $_POST['email'];
+			$middleInitial = NULL;
+			$primaryPhone = $_POST['phone'];
+			$secondaryPhone = NULL;
+			$city = $_POST['city'];
+			$county = NULL;
+			$state = $_POST['state'];;
+			$zip = $_POST['zipcode'];
+			$dob1 = $_POST['DOBYear'] . '-' . $_POST['DOBMonth'] . '-' . $_POST['DOBDay'];
+			$dob2 = DateTime::createFromFormat('Y-m-d' , $dob1);
+			$dob = $dob2->format('Y-m-d');
+			$street = $_POST['address'];
+			$status = 'Applicant';
+			/* $rabiesVac1 = $_POST['VACYear'] . '-' . $_POST['VACMonth'] . '-' . $_POST['VACDay'];
+			$rabiesVac2 = DateTime::createFromFormat('Y-m-d' , $rabiesVac1);
+			$rabiesVac = $rabiesVac2->format('Y-m-d'); */
+			$lastVolunteered = NULL;
+			$allergies = NULL;
+			$specialNeeds = NULL;
+			$workOutside = NULL;
+			$totalHours = NULL;
+			$workOutsideLimitations = NULL;
+			$lift40 = NULL;
+			$permitRehab = NULL;
+			$travel = $_POST['travel'];
+			$capture = $_POST['capture'];
+			$acknowledge = $_POST['acknowledge'];
+			if ($permitRehab < 1)
+			{
+				$permitRehab = NULL;
+			}
+$query = "INSERT INTO person (Person_UserName, Person_PasswordHash, Person_UserType, Person_FirstName, Person_MiddleName, Person_LastName, Person_Email, Person_PhonePrimary, Person_PhoneAlternate, Person_StreetAddress, Person_City, Person_County, Person_State, Person_Country, Person_ZipCode, Person_DateOfBirth, Person_Status, Person_RabbiesVaccinationDate, Person_RehabilitatePermitCategory, Person_Allergies, Person_SpecialNeeds,
+		  Person_WorkOutside, Person_OutsideLimitations, Person_Lift40Lbs, Person_TotalVolunteeredHours, Person_LastVolunteered)
+			  VALUES ('$userName', '$passwordHash', '$userType', '$firstName', NULL, '$lastName', '$email', '$primaryPhone', NULL, '$street', '$city', NULL, '$state', NULL, '$zip', '$dob', '$status', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)";
+
+
+
+		if(!mysqli_query($conn,$query))
+
+			{
+				echo("Error description: " . mysqli_error($conn));
+			}
+
+			else
+			{
+				echo "Application Sent! {person table}";
+			}
+			
+			$depNum = 4;
+			$sql = "SELECT MAX(Person_ID) FROM Person";
+			$result = $conn->query($sql);
+			$personID = null;
+			if($result->num_rows > 0) {
+				//output data of each row
+				while($row = $result->fetch_assoc()) {
+					$personID = $row['MAX(Person_ID)'];
+				}
+			}
+			
+			$transporterQuery = "INSERT INTO Transporterapp (TransporterApp_PersonID, TransporterApp_HandsOnExperience, TransporterApp_HandleDeadAnimals, TransporterApp_OpinionLivePrey)
+			  VALUES ('$personID', '$travel', '$capture', '$acknowledge')";
+
+
+
+			  if(!mysqli_query($conn,$transporterQuery))
+
+			{
+				echo("Error description: " . mysqli_error($conn));
+			}
+
+			else
+			{
+				echo "Application Sent! {TransporterApp table}";
+			}
+			
+			$fileName = $_FILES['rabbiesDocumentation']['name'];
+			$tmpName  = $_FILES['rabbiesDocumentation']['tmp_name'];
+			$fileSize = $_FILES['rabbiesDocumentation']['size'];
+			$fileType = $_FILES['rabbiesDocumentation']['type'];
+			$fp      = fopen($tmpName, 'r');
+			$content = fread($fp, filesize($tmpName));
+			$content = addslashes($content);
+			fclose($fp); 
+			
+			$documentQuery = "INSERT INTO Documentation (Documentation_PersonID, Documentation_TypeOfDocument, Documentation_FileName, Documentation_FileType, Documentation_FileContent, Documentation_DocumentNotes)
+			  VALUES ('$personID', 'Proof of Vaccination', '$fileName', '$fileType', '$content', NULL)";
+
+
+
+			  if(!mysqli_query($conn,$documentQuery))
+
+			{
+				echo("Error description: " . mysqli_error($conn));
+			}
+
+			else
+			{
+				echo "Document Sent!";
+			}
+		}
+	}
+			?>
 
 
 		
