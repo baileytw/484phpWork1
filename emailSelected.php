cef40cf3bd43c973996f142ff248788244945f37
matts90 <seilermr@dukes.jmu.edu>
2017-04-07 00:35:49 +0000

    email

--- emailSelected.php
+++ emailSelected.php
@@ -17,9 +17,43 @@ if ($userTypeSession != "Team Lead"){
 
 */
 
+if(isset($_POST['btnLogIn'])){
+	require 'C:\inetpub\wwwroot\PHPMailer\PHPMailerAutoload.php';
+	
+	//DO A LOOP TO POPULATE EMAIL ADDRESS AND SEND EMAIL FOR EACH ITERATION
+	$emailAddress = 'seilermr@dukes.jmu.edu';
+	$mail = new PHPMailer;
+	
 
+	//$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
+	$mail->isSMTP();                                      // Set mailer to use SMTP
+	$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+	$mail->SMTPAuth = true;                               // Enable SMTP authentication
+	$mail->Username = 'wcvtestemail@gmail.com';                 // SMTP username
+	$mail->Password = '1wildcva';                           // SMTP password
+	$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+	$mail->Port = 587;                                    // TCP port to connect to
 
+	$mail->setFrom('wcvtestemail@gmail', 'Wildlife Center of Virginia');
+	$mail->addAddress($emailAddress);     // recipient
+	//$mail->addAddress('ellen@example.com');               //Add team lead
+
+	$mail->Subject = '';
+	$mail->Body    = ' '; 
+	
+	$mail->AltBody = '';
+
+	
+	$mail->isHTML(true);                                  // Set email format to HTML
+	
+	if(!$mail->send()) {
+		echo 'Message could not be sent.';
+		echo 'Mailer Error: ' . $mail->ErrorInfo;
+	} else {
+		echo 'Message has been sent';
+	}
+}
 
 
 ?>
